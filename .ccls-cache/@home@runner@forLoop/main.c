fefee31192f37a2b0d159a59f9c7edb117a59133
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-01 08:52:34 +0000

    add reverse number

--- .ccls-cache/@home@runner@forLoop/main.c
+++ .ccls-cache/@home@runner@forLoop/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  for(int i = 0; i <= 10; i++) {
-    printf("%d\n",i);
+  int num, sum = 0;
+  printf("Enter the number: ");
+  scanf("%d", &num);
+  printf("\n");
+  for (int i = 1; i <= num; i++) {
+    sum += i;
+  }
+  printf("%d\n", sum);
+  printf("\n");
+
+  for(int i = num; i >= 1; i--) {
+    printf("%d\n", i);
   }
   return 0;
 }
